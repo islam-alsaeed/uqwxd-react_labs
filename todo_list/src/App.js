cd10f8247a09669801cdac386eeaf6dd421f9d10
Sam <islam.alsaeed@gmail.com>
2023-12-04 11:04:21 +0200

    add task functionality to todo app

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -4,7 +4,23 @@ const App = () => {
   const [todos, setTodos] = useState([]);
   
   // Add the handlesubmit code here
-  
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let todo = document.getElementById('todoAdd').value
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    };
+    if (newTodo.text.length > 0 ) {
+        setTodos([...todos].concat(newTodo));
+    } else {
+
+        alert("Enter Valid Task");
+    }
+    document.getElementById('todoAdd').value = ""
+  }
   
   // Add the deleteToDo code here
 
@@ -16,12 +32,20 @@ const App = () => {
 
   
 return(
-<div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" id= 'todoAdd'/>
-<button type ="submit">Add Todo</button>
-</form>
+<div id="todo-list">
+    <h1>Todo List</h1>
+        <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              id = 'todoAdd'
+            />
+            <button type="submit">Add Todo</button>
+        </form>
+        {todos.map((todo) =>
+            <div className="todo" key={todo.id}>
+                <div className="todo-text">{todo.text}</div>
+            {/* insert delete button below this line */}
+            </div>)}
 </div>
 );
 };
